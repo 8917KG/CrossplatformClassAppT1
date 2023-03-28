22cc80d4ee18626b7b0f97466b55379f91dc58b9
8917KG <8917@ait.nsw.edu.au>
2023-03-28 16:39:02 +1100

    adding home and sign up screens

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//screens
+import { HomeScreen } from './Screens/HomeScreen';
+import { SignUpScreen } from './Screens/SignUp';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+        <Stack.Navigator>
+          <Stack.Screen name = "SignUp" component={SignUpScreen}/>
+          <Stack.Screen name = "Home" component={HomeScreen}/>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ Screens/SignUp.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function SignUpScreen (props){
+    return(
+        <View>
+            <Text>
+                Sign Up Screen
+            </Text>
+        </View>
+    )
+}
